81a1764f691a1355180f75a3e9aca97515cc34e2
Rodrigo Ruiz Baca <rodrigo.ruiz.baca@gmail.com>
2018-08-03 17:16:25 -0500

    Implementation

--- src/main/java/coding/bigsum/BigSum.java
+++ src/main/java/coding/bigsum/BigSum.java
@@ -2,7 +2,7 @@ package coding.bigsum;
 
 import java.util.Arrays;
 
-public class BigSum {
+public class BigSum implements BigSumI {
 	public long bigSum(long[] l) {
 		return Arrays.stream(l).parallel().reduce((acum, val) -> acum = acum + val).getAsLong();
 	}
---
+++ src/main/java/coding/bigsum/BigSumI.java
@@ -0,0 +1,5 @@
+package coding.bigsum;
+
+public interface BigSumI {
+	public long bigSum(long[] l);
+}
