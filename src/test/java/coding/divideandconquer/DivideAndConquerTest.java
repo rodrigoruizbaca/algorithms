32236fabf8d86bf5d9fd77f6ee687c6dc982c114
Rodrigo Ruiz Baca <rodrigo.ruiz.baca@gmail.com>
2018-09-24 16:01:35 -0500

    x

--- src/test/java/coding/divideandconquer/DivideAndConquerTest.java
+++ src/test/java/coding/divideandconquer/DivideAndConquerTest.java
@@ -3,15 +3,9 @@ package coding.divideandconquer;
 import static org.junit.Assert.assertEquals;
 
 import java.lang.reflect.Constructor;
-import java.util.concurrent.TimeUnit;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
-import org.openjdk.jmh.runner.options.TimeValue;
 
 public class DivideAndConquerTest {
 	
@@ -45,27 +39,5 @@ public class DivideAndConquerTest {
 		assertEquals(21, resp);
 	}
 	
-	@Test 
-	public void launchBenchmark() throws Exception {
-		Options opt = new OptionsBuilder()
-                // Specify which benchmarks to run. 
-                // You can be more specific if you'd like to run only one benchmark per test.
-                .include(this.getClass().getName() + ".*")
-                // Set the following options as needed
-                .mode (Mode.AverageTime)
-                .timeUnit(TimeUnit.MICROSECONDS)
-                .warmupTime(TimeValue.seconds(1))
-                .warmupIterations(2)
-                .measurementTime(TimeValue.seconds(1))
-                .measurementIterations(2)
-                .threads(2)
-                .forks(1)
-                .shouldFailOnError(true)
-                .shouldDoGC(true)
-                //.jvmArgs("-XX:+UnlockDiagnosticVMOptions", "-XX:+PrintInlining")
-                //.addProfiler(WinPerfAsmProfiler.class)
-                .build();
-
-            new Runner(opt).run();
-	}
+	
 }
